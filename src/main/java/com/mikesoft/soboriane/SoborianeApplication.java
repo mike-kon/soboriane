e5bcb84c75bb453098feabb5a152d81640d03cc0
Кононович Михаил Александрович <makononovich@dev.vtb.ru>
2026-03-23 00:36:28 +0300

    init

---
+++ src/main/java/com/mikesoft/soboriane/SoborianeApplication.java
@@ -0,0 +1,13 @@
+package com.mikesoft.soboriane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoborianeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoborianeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mikesoft/soboriane/SoborianeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikesoft.soboriane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoborianeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
