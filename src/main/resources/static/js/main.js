64d1ba68261f21670e2950848e7260ae12c32991
Кононович Михаил Александрович <makononovich@dev.vtb.ru>
2026-03-30 22:34:53 +0300

    debug on computer view

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,8 @@
-
 function clickMainButton(div) {
     if ($(div).hasClass("button_press")) {
         return;
     }
-    $(".button_press").each(function(i, elem) {
+    $(".button_press").each(function (i, elem) {
         $(elem).removeClass("button_press");
         $(elem).addClass("button_unpress")
     });
@@ -19,3 +18,71 @@ function loadData(tag) {
 function sendMessage() {
     console.log("Under construction");
 }
+
+function updateTextByWidth() {
+    $('.dynamic-title').each(function () {
+        updateTextByWidthElement(this);
+        });
+}
+
+function updateTextByWidthElement(elem) {
+    const dataname = ['data-full','data-medium','data-small'];
+    const fullWidth = $(elem).width();
+    const elems = $(elem).children('.dynamic-elem');
+    const chLen = elems.length;
+    let arr = new Array();
+    // инициализация
+    for (i=0 ; i< chLen; i++) {
+        arr[i] = 0;
+    }
+    // подбор
+    tooWide = true;
+    tooSmall = false;
+    cur=0;
+    while (tooWide) {
+        w = 0;
+        for( i=0; i< chLen; i++) {
+            elems[i].textContent = elems[i].getAttribute(dataname[arr[i]]);
+            w += $(elems[i]).width();
+        }
+        if (w < fullWidth) {
+            tooWide = false;
+        } else {
+            arr[cur++]++;
+            if (cur >= chLen) {
+                cur = 0;
+            }
+            if (arr[0] >= dataname.length) {
+                tooSmall = true;
+                tooWide = false;
+            }
+        }
+    }
+    if (tooSmall) {
+        for( i=0; i< chLen; i++) {
+            elems[i].textContent = '';
+        }
+    }
+}
+
+function getTextWidthFromElement($element, text) {
+    // Получаем стили элемента
+    const fontSize = $element.css('fontSize');
+    const fontFamily = $element.css('fontFamily');
+    const font = `${fontSize} ${fontFamily}`;
+
+    // Измеряем текст
+    const canvas = $('<canvas></canvas>')[0];
+    const context = canvas.getContext('2d');
+    context.font = font;
+
+    return context.measureText(text || $element.text()).width;
+}
+
+function init() {
+    updateTextByWidth();
+    $('#messageCanvas').scrollTop(1000);
+}
+
+window.addEventListener('load', init);
+window.addEventListener('resize', init);
