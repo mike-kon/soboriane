4390a124f1e9615c7204d8187d3cac439f2edd89
Кононович Михаил Александрович <makononovich@dev.vtb.ru>
2026-04-05 17:43:19 +0300

    desine completed

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,6 @@
+// Ширина, меньше которой включается механизм меню в кнопке
+const MENU_BUTTON_WIDTH = 500;
+
 function clickMainButton(div) {
     if ($(div).hasClass("button_press")) {
         return;
@@ -22,26 +25,26 @@ function sendMessage() {
 function updateTextByWidth() {
     $('.dynamic-title').each(function () {
         updateTextByWidthElement(this);
-        });
+    });
 }
 
 function updateTextByWidthElement(elem) {
-    const dataname = ['data-full','data-medium','data-small'];
+    const dataname = ['data-full', 'data-medium', 'data-small'];
     const fullWidth = $(elem).width();
     const elems = $(elem).children('.dynamic-elem');
     const chLen = elems.length;
     let arr = new Array();
     // инициализация
-    for (i=0 ; i< chLen; i++) {
+    for (i = 0; i < chLen; i++) {
         arr[i] = 0;
     }
     // подбор
     tooWide = true;
     tooSmall = false;
-    cur=0;
+    cur = 0;
     while (tooWide) {
         w = 0;
-        for( i=0; i< chLen; i++) {
+        for (i = 0; i < chLen; i++) {
             elems[i].textContent = elems[i].getAttribute(dataname[arr[i]]);
             w += $(elems[i]).width();
         }
@@ -59,12 +62,33 @@ function updateTextByWidthElement(elem) {
         }
     }
     if (tooSmall) {
-        for( i=0; i< chLen; i++) {
+        for (i = 0; i < chLen; i++) {
             elems[i].textContent = '';
         }
     }
 }
 
+function menuClick(event) {
+    const windowWidth = $(window).width();
+    if (windowWidth < MENU_BUTTON_WIDTH) {
+        event.stopPropagation();
+        $('#menuContext').css('display', 'block');
+    }
+}
+
+function menuUnClick() {
+    const windowWidth = $(window).width();
+    if (windowWidth < MENU_BUTTON_WIDTH) {
+        $('#menuContext').css('display', '');
+    }
+}
+
+function canvasClear() {
+    menuUnClick();
+}
+
+// @Depricated
+/*
 function getTextWidthFromElement($element, text) {
     // Получаем стили элемента
     const fontSize = $element.css('fontSize');
@@ -78,8 +102,15 @@ function getTextWidthFromElement($element, text) {
 
     return context.measureText(text || $element.text()).width;
 }
+*/
 
 function init() {
+    $('#main').on('click', function () {
+        canvasClear();
+    });
+    $('#menu').on('click', function (event) {
+        menuClick(event);
+    });
     updateTextByWidth();
     $('#messageCanvas').scrollTop(1000000);
 }
